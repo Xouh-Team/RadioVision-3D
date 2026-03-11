6f0f747ac60f3cde49cd369e3a6dba5a77384f68
Hypotex <thomas.roger@epitech.eu>
2026-03-11 17:00:38 +0100

    feat: create github page for the project

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/RadioVision-3D/',
   plugins: [react()],
   build: {
     sourcemap: false,
